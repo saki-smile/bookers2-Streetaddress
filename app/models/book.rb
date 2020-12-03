7bd340e298d5b5e452dc54a77d4d9a8e144debbb
saki-smile <sa.su.ko0419@gmail.com>
2020-12-03 09:17:41 +0000

    [add] relationships_controller

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,6 +8,6 @@ class Book < ApplicationRecord
 
 	def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
-   end
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,24 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :following_user, through: :follower, source: :followed
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :follower_user, through: :followed, source: :follower
+  
   attachment :profile_image, destroy: false
+  #フォローする
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+  #フォローを外す
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+  #フォローしていればtrueを返す
+  def following?(user)
+    following_user.include?(user)
+  end
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: { maximum: 50}
