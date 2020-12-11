e080fba2a40087a13c4c777a53b6383d7336a3cb
saki-smile <sa.su.ko0419@gmail.com>
2020-12-11 14:50:22 +0000

    [add} seach bootstrap

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -52,10 +52,10 @@ class BooksController < ApplicationController
   end
 
   def ensure_correct_user
-    @book = PostComment.find(params[:id])
+    @book = Book.find(params[:id])
     unless @book.user == current_user
       redirect_to books_path
     end
   end
-  
+
 end
